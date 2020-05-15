777e3d899a0aa5f97a6d31a51287619c9510d592
sofya-on <47686226+sofya-on@users.noreply.github.com>
2020-05-15 18:02:22 +0300

    Update inversemat.m

--- inversemat.m
+++ inversemat.m
@@ -10,10 +10,10 @@ function retval = inversemat (matrix1)
       a2_1 = matrix1(i+1,1:i);
       a1_2 = matrix1(1:i,i+1);
       a2_2 = matrix1(i+1,i+1);
-      X = a1_1*a1_2;
-      Y = a2_1*a1_1;
-      U = a2_2 - Y*a1_2;
-      a1_1 = [a1_1 + X*(1/U)*Y, -X*(1/U);-(1/U)*Y, 1/U];
+      B = a1_1*a1_2;
+      C = a2_1*a1_1;
+      U = a2_2 - C*a1_2;
+      a1_1 = [a1_1 + B*(1/U)*C, -B*(1/U);-(1/U)*C, 1/U];
     end;
   retval = a1_1;
   endif;
