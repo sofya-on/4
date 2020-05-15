15846fd319a103ee1b5625ad54b98dbea506958b
sofya-on <47686226+sofya-on@users.noreply.github.com>
2020-05-15 17:41:13 +0300

    Update LABA4_2.m

--- LABA4_2.m
+++ LABA4_2.m
@@ -1,32 +1,7 @@
-## Copyright (C) 2020 nikit
-## 
-## This program is free software: you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see
-## <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} LABA4_2 (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: nikit <nikit@LAPTOP-F2VC9HFF>
-## Created: 2020-04-12
-
 function retval = LABA4_2 (matrix1)
   len = length(matrix1);
   if (abs(det(matrix1)) < 0.0000000000001)
-    retval = "Íå ñóùåñòâóåò";
+    retval = "ÃÃ¥ Ã±Ã³Ã¹Ã¥Ã±Ã²Ã¢Ã³Ã¥Ã²";
     return;
   else
     matrix1(1,1) = 1/matrix1(1,1);
